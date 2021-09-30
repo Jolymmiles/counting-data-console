1084a5de63620ad87f1d6ea650876a1a17642212
Валентин <valmak04@gmail.com>
2021-09-30 23:13:01 +0400

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args) {
-        //write your code here
+        boolean Check_Flag = true;
+        Scanner sc = new Scanner(System.in);
+        double sum = 0;
+        int counter = -1;
+        while (Check_Flag) {
+            int input_digit = sc.nextInt();
+            if (input_digit == 0) {
+                Check_Flag = false;
+            }
+            sum += input_digit;
+            counter += 1;
+        }
+        System.out.println(sum/counter);
     }
 }
